c1039c1447e7f1aeec59401b0da7af6d4edb77f9
Fernando <fernanzambrano@gmail.com>
2016-09-14 22:04:50 +0200

    First executable version

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.directives','app.services',])
+angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.directives','app.services', ])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -19,5 +19,101 @@ angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.directives
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
+
+    setupPush();
+
   });
 })
+
+this.setupPush = function () {
+  if (localStorage.senderID == '[object Object]') {
+    //localStorage.senderID = "918469035747";  
+    localStorage.senderID = "";
+  }  
+
+  if (localStorage.notifications == '[object Object]') {
+    localStorage.notifications = "[]";
+  }
+
+  var push = PushNotification.init({
+     "android": {
+         "senderID": localStorage.senderID
+     },
+     "ios": {
+       "sound": true,
+       "alert": true,
+       "badge": true
+     },
+     "windows": {}
+  });
+
+
+  push.on('registration', function(data) {
+     console.log("registration event: " + data.registrationId);
+     var oldRegId = localStorage.registrationId;
+     if (oldRegId !== data.registrationId) {
+         // Save new registration ID
+         localStorage.registrationId = data.registrationId;
+         // Post registrationId to your app server as the value has changed
+         console.log("a new regID has been archived");
+     }
+  });
+
+  push.on('notification', function(data) {
+         console.log("notification event");
+
+         var notifications = [];
+
+         if (localStorage.notifications != '[object Object]') {
+         //   console.log("FZG ********** recuperando desde localStorage");
+            notifications = JSON.parse(localStorage.notifications);
+
+            //$scope.notifications.push(JSON.stringify(notification.alert));
+         }
+
+         var notification = {};
+
+         notification.time = Date.now();
+         notification.message = data.message;
+         notification.title = data.title;
+
+         notifications.push(notification);
+
+         localStorage.notifications = JSON.stringify(notifications);
+
+         locationMedia = "";
+         if (device.platform == "Android"){
+            locationMedia = "/android_asset";
+         }
+         locationMedia = locationMedia + "/www/sound/beep.wav";
+
+         playSound(locationMedia);
+
+         window.plugins.toast.showShortTop('A new notification has been received.');
+
+     }); 
+
+  push.on('error', function(e) {
+     console.log("push error during setup  = " + e.message);
+  });
+};
+
+this.playSound = function(mediaFile) {
+  var my_media = new Media(mediaFile,
+    // success callback
+    function () {
+        console.log("playAudio():Audio Success");
+    },
+    // error callback
+    function (err) {
+        console.log("playAudio():Audio Error: " + err);
+    }
+  );
+  // Play audio
+  my_media.play();
+};
+
+
+
+
+
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,11 +1,37 @@
 angular.module('app.controllers', [])
      
-.controller('listOfNotificationsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('listOfNotificationsCtrl', ['$scope', '$stateParams',  // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
 
+		this.listnot = JSON.parse(localStorage.notifications);
 
+		setInterval(function(){ document.getElementById('listnot-button1').click(); }, 3000);
+
+		this.refreshNotifications = function () {
+			this.listnot = JSON.parse(localStorage.notifications);
+		};
+
+		this.deleteNotifications = function () {
+
+          navigator.notification.confirm(
+              'You are going to delete all notifications. Are you sure?', // message
+               onConfirm,            // callback to invoke with index of button pressed
+              'Delete',           // title
+              ['Yes','No']     // buttonLabels
+          );
+
+         function onConfirm(buttonIndex) {
+         	if (buttonIndex == 1) {
+				this.listnot = [];
+				//localStorage.setItem('notifications', JSON.stringify(this.listnot));
+				localStorage.notifications = JSON.stringify(this.listnot);
+				window.plugins.toast.showShortBottom('Notifications have been removed.');      		
+         	}
+            //alert('You selected button ' + buttonIndex);
+         }
+		};
 }])
    
 .controller('deviceInfoCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
@@ -13,14 +39,25 @@ function ($scope, $stateParams) {
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
 
+	this.regID = localStorage.registrationId;
+	this.platform = device.platform;
+
+	this.registerDevice = function() {
+		//Here we have to include the opeation to register de regID in the external server
+	};
 
 }])
-   
+
+
 .controller('configurationCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
 
+	this.senderID = localStorage.senderID;
 
-}])
-    
+	this.registerConfiguration = function() {
+		localStorage.senderID = this.senderID;
+	};
+
+}]) 
